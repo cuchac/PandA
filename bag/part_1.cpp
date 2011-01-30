1eb1150f09e9b82ccb14303afe8f64d4105acb0f
cuchac <cuchac@email.cz>
2011-01-30 22:48:54 +0100

    Fix part 1 + steps counting

--- bag/part_1.cpp
+++ bag/part_1.cpp
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <algorithm>
+#include <math.h>
 #include "main.h"
 #include "util.h"
 
@@ -12,17 +13,13 @@ int strategy_BrutalForce(instance &sInstance)
 
    strategy_BrutalForce_recurse(sInstance, sCurrentSolution, 0);
 
-   if(sCurrentSolution.price != 0)
-      std::cout << "Algorythm error! sCurrentSolution.price:" << sCurrentSolution.price;
-
-   if(sCurrentSolution.weight != 0)
-      std::cout << "Algorythm error! sCurrentSolution.weight:" << sCurrentSolution.weight;
-
    return 0;
 }
 
 int strategy_BrutalForce_recurse(instance &sInstance, solution &sCurrentSolution, uint uiLevel)
 {
+   g_uiStates++;
+
    if(sCurrentSolution.weight <= sInstance.capacity && sInstance.result.price < sCurrentSolution.price)
       sInstance.result = sCurrentSolution;
 
@@ -33,7 +30,6 @@ int strategy_BrutalForce_recurse(instance &sInstance, solution &sCurrentSolution
       sCurrentSolution.add(sItem);
       strategy_BrutalForce_recurse(sInstance, sCurrentSolution, uiLevel+1);
       sCurrentSolution.del(sItem);
-
       strategy_BrutalForce_recurse(sInstance, sCurrentSolution, uiLevel+1);
    }
 
@@ -47,33 +43,12 @@ int strategy_Greedy(instance &sInstance)
    std::sort(sInstance.items.begin(), sInstance.items.end(), item::sorter_by_quality);
 
    for(items_t::iterator it = sInstance.items.begin(); it != sInstance.items.end() && (sInstance.result.weight + it->weight <= sInstance.capacity); it++)
-      sInstance.result.add(*it);
-
-   std::sort(sInstance.items.begin(), sInstance.items.end(), item::sorter_by_id);
-
-   return 0;
-}
-
-/*
-int strategy_brutal_force_recurse(instance &sInstance, solution &sCurrentSolution, uint uiLevel)
-{
-   for(uint uiIndex = 0; uiIndex < sCurrentSolution.selection.size(); uiIndex++)
    {
-      item sItem = sInstance.items[uiIndex];
-      if(!sCurrentSolution.selection[uiIndex] && sCurrentSolution.weight + sItem.weight <= sInstance.capacity)
-      {
-         sCurrentSolution.add(sItem);
-
-         print_solution(sCurrentSolution, 0);
-
-         if(sMaxPriceSolution.price < sCurrentSolution.price)
-            sMaxPriceSolution = sCurrentSolution;
-
-         strategy_brutal_force_recurse(sInstance, sCurrentSolution);
-
-         sCurrentSolution.del(sItem);
-      }
+      g_uiStates++;
+      sInstance.result.add(*it);
    }
+
+   //std::sort(sInstance.items.begin(), sInstance.items.end(), item::sorter_by_id);
+
    return 0;
 }
-*/
